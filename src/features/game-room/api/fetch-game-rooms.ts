ea2e48a0996732edb240a4b40b9d89f1734be948
hjh79gw <hjh79gw@gmail.com>
2026-03-30 09:38:01 +0900

    fix(gameRobby): chage base url api fetch

--- src/features/game-room/api/fetch-game-rooms.ts
+++ src/features/game-room/api/fetch-game-rooms.ts
@@ -4,12 +4,12 @@ import type { CreateRoomRequest } from '../model/game-room.schema'
 import { createRoomResponseSchema, roomListResponseSchema } from '../model/game-room.schema'
 
 export async function fetchGameRooms() {
-  const response = await baseApi.get('/api/rooms')
+  const response = await baseApi.get('/rooms')
   //const response = await baseApi.get('/mock/game-rooms.json')
   return roomListResponseSchema.parse(response.data)
 }
 
 export async function createGameRoom(request: CreateRoomRequest) {
-  const response = await baseApi.post('/api/rooms', request)
+  const response = await baseApi.post('/rooms', request)
   return createRoomResponseSchema.parse(response.data)
 }
